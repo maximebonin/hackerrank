8d110b5a9f64d988a59d0b00b01661983360fdf6
maximebonin <bonin.maxime@gmail.com>
2016-09-24 16:46:40 -0400

    Java Output Formatting
    intro to java section of Hackerrank

---
+++ javaOutputFormatting.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i  )
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                
+                
+                String space="";
+                
+                for(int j = 0 ; j< 15 - s1.length() ; j  )
+                {
+                    space  =" ";
+                }
+                
+                String formatted = String.format("d", x);
+                
+                
+                System.out.println(s1 space formatted);
+            }
+            System.out.println("================================");
+
+    }
+}
