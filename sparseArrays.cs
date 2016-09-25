194198058eb6ea56ede5ad675ea9d691b0dd28e8
maximebonin <bonin.maxime@gmail.com>
2016-09-24 23:17:23 -0400

    sparseArray
    Hackerrank’s data structures challenge in c#

--- sparseArrays.cs
+++ sparseArrays.cs
@@ -4,18 +4,13 @@ using System.IO;
 class Solution {
     
     
-    private static bool searching(string p_Data, string p_Query)
-    {
-        return p_Data == p_Query;
-    }
-    
     static void Main(String[] args) 
     {
         
         string aa = Console.ReadLine();
         int a = Int32.Parse(aa);
             
-        string[] data = new string[1000];
+        string[] data = new string[a];
         
         for (int i = 0 ; i < a ; i​++​  )
         {
@@ -24,7 +19,7 @@ class Solution {
         string bb = Console.ReadLine();
         int b = Int32.Parse(bb);
         
-        string[] query = new string[1000];
+        string[] query = new string[b];
         
         for(int i = 0 ; i < b ; i​++​  )
         {
@@ -37,9 +32,9 @@ class Solution {
             
             for(int j = 0 ; j < a ; j​++  )
             {
-                if(searching(data[j], query[i]))
+                if(data[i] == query[i])
                 {
-                    howMuch  ;
+                    howMuch++;
                 }
             }
            Console.WriteLine(howMuch);
