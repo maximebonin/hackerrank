171253dc96fa1fcce37a8aa13f4aa34cdcd514d2
maximebonin <bonin.maxime@gmail.com>
2016-10-10 18:15:38 -0400

    Day 27 : testing
    Hackerrank’s day 27 : Testing in C#

---
+++ day27Testing.cs
@@ -0,0 +1,21 @@
+    // this one is tricky
+    // I only figured out what they wanted
+    // after coding the whole thing ...
+    
+    
+    
+    
+    static void Main(String[] args) 
+    {
+        
+        Console.WriteLine(2);
+        
+        Console.WriteLine("4 1");
+        
+        Console.WriteLine("-1 -2 0 1");
+
+        Console.WriteLine("5 2");
+        
+        Console.WriteLine("2 3 4 0 -3");
+        
+    }
