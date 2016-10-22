c29f91d9801fb914b8adf6830a0e1a5c348e9d3f
maximebonin <bonin.maxime@gmail.com>
2016-10-22 16:37:12 -0400

    Python Find the Second Largest Number

---
+++ pythonFindSecondLargestNumber.py
@@ -0,0 +1,23 @@
+howMany = int(raw_input())
+
+stringInput = raw_input()
+
+listNumbers = stringInput.split()
+
+listNumbers = [int(x) for x in listNumbers]
+
+listNumbers.sort()
+
+largestNumber = -99999999
+secondLargestNumber = -99999999
+
+for x in listNumbers[::-1]:  # the loop is in reverse order
+    if(x >= largestNumber):
+        largestNumber = x
+    else:
+        if(x > secondLargestNumber):
+            secondLargestNumber = x
+        else:
+            break
+            
+print(secondLargestNumber)
