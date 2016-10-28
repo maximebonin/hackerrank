f1424c7f6210c008fdd79401e7c55e9133c2a3ae
maximebonin <bonin.maxime@gmail.com>
2016-10-28 15:10:38 -0400

    Doshi's answer

--- pythonFindAString.py
+++ pythonFindAString.py
@@ -14,3 +14,19 @@ for x in range (rangeX):
         counter += 1
 
 print(counter)
+
+# DOSHI's answer
+# He slices the string into a substring 
+# of the length of the given substring
+# and compares it with the substring
+
+
+
+A = raw_input().strip()
+x = raw_input().strip()
+
+count = 0
+for i in range(len(A) - len(x) + 1):
+    if A[i:i+len(x)] == x:
+        count += 1
+print count
